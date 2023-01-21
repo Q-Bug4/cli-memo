b8db23fe8157dbc844b1ca3d7a396c336f3fc1e6
wulnm <wumqing@yeah.net>
2023-01-21 18:51:25 +0800

    feat: add sqlite

---
+++ src/bin/show_memos.rs
@@ -0,0 +1,20 @@
+use diesel::prelude::*;
+use climemo::establish_connection;
+use climemo::models::Memo;
+
+fn main() {
+    use climemo::schema::memos::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = memos
+        .limit(5)
+        .load::<Memo>(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for memo in results {
+        println!("{}", memo.name);
+        println!("-----------\n");
+        println!("{}", memo.content);
+    }
+}
