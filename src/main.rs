e1ed5ab339a6c78cd08127ddab3d64e49a85d3ee
wulnm <wumqing@yeah.net>
2023-01-24 23:28:56 +0800

    refactor: move enum code into enums.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 #![allow(unused)]
 
-mod constant;
-
 use structopt::StructOpt;
-use crate::memo::Memo;
-use crate::constant::{InputSourceEnum, Language, OutputResultEnum};
 
 #[derive(StructOpt)]
 struct Cli {
